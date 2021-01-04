c22c0283b040e65aff277fbb1882058825fd6087
The Professor <contact.pr0f3ss0r@gmail.com>
2021-01-04 22:14:33 +0530

    ✔ FIXED LOGIN ISSUE

--- myigbot.py
+++ myigbot.py
@@ -180,7 +180,7 @@ class MyIGBot:
 
             self.sessionid = login_response.headers['Set-Cookie'].split('sessionid=')[1].split(';')[0]                
             self.userId =   login_response.headers['Set-Cookie'].split('ds_user_id=')[1].split(';')[0]              
-            self.cookie = "sessionid=" + self.sessionid + "; csrftoken=" + self.csrf_token + ";" + 'ds_user_id=' + self.userId + ";"
+            self.cookie = "sessionid=" + self.sessionid + "; csrftoken=" + self.csrf_token + "; ds_user_id=" + self.userId + ";"
             create_cookie = open(self.path+f'//cookie_{self.username}.bot', 'w+', encoding='utf-8')
             create_cookie.write(self.cookie)
             create_cookie.close()
